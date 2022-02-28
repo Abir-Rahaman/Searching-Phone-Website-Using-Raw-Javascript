2add20b4c88ddd95bd886637833296f7c41db62e
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-03-01 03:20:14 +0600

    phone  details section done

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const loadData = () =>{
 }
 
 // show phone sector using (arrow function & forEach loop)
-const displayData = (phones) =>{
-    console.log(phones)
+const displayData = phones =>{
+    // console.log(phones)
     const phoneCard =document.getElementById('phone-card');
     phones.forEach( phone => {
         const div = document.createElement('div');
@@ -21,9 +21,9 @@ const displayData = (phones) =>{
                         <div class="col-12 d-flex justify-content-center align-items-center">
                             <img class=" mx-auto" src="${phone.image}" class="card-img-top" alt="...">
                             <div class="card-body ">
-                                <h5 class="card-title ">  ${phone.phone_name}</h5>
+                                <h4 class="card-title ">  ${phone.phone_name}</h4>
                                 <h6 class="card-title "> Brand Name :${phone.brand}</h6>  
-                                <button onclick="" class="btn btn-outline-secondary" type="button" id="search-button">Details</button>  
+                                <button onclick="loadDetails('${phone.slug}')" class="btn btn-outline-secondary" type="button" id="search-button">Details</button>  
                             </div>
                         </div>
                     </div>`;
@@ -34,9 +34,37 @@ const displayData = (phones) =>{
 
 }
 
- 
-const loadDetails = (details) => {
+// calling API with arrow function to display details of phone
+const loadDetails = details => {
     const url = `https://openapi.programming-hero.com/api/phone/${details}`
+   
     fetch(url)
     .then (res => res.json())
+    .then (data => displayDetails(data.data));
+}
+
+// show phone details sector using arrow function & object
+const displayDetails = info =>{
+    console.log(info)
+    const moreDetails = document.getElementById('more-details')
+    moreDetails.innerHTML = `<div class="col-md-6 ">
+                            <div class=" card rounded-3  mb-3 p-4 w-50 mx-auto border-0 " style ="background :rgb(247, 241, 231)" >
+                                <img class="w-25  mx-auto" src="${info.image}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h4 class="card-title ">${info.name}</h4>
+                                <h5 class="card-title">Release Date : ${info.releaseDate}</h5>
+                                
+                                <h5 class="card-title "> Main Features :- </h5> 
+                                    <ul>
+                                    <li> ${info.mainFeatures.chipSet} </li>
+                                    <li>${info.mainFeatures.displaySize}</li>
+                                    <li>${info.mainFeatures.memory}</li>
+                                </ul>
+                                <button onclick="" class="btn btn-outline-secondary " type="button"> Sensor info </button>
+                                <button onclick="" class="btn btn-outline-secondary" type="button"> Others info</button>
+                               
+                                </div>
+                                </div>
+                                </div>`; 
+
 }

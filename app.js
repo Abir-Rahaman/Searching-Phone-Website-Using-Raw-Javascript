0c3b97488bd40705a2f9607e366775d5476d025d
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-03-01 17:45:03 +0600

    sensor part done

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 // calling API with arrow function
-const loadData = () =>{ 
-    document.getElementById('phone-card').innerHTML='';
+const loadData = () => {
+    document.getElementById('phone-card').innerHTML = '';
 
-    const inputField =document.getElementById('input-field').value;
+    const inputField = document.getElementById('input-field').value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputField}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayData(data.data))
-    
-
+        .then(res => res.json())
+        .then(data => displayData(data.data))
 }
 
 // show phone sector using (arrow function & forEach loop)
-const displayData = phones =>{
-    // console.log(phones)
-    const phoneCard =document.getElementById('phone-card');
-    phones.forEach( phone => {
+const displayData = phones => {
+    const phoneCard = document.getElementById('phone-card');
+    phones.forEach(phone => {
         const div = document.createElement('div');
-        div.innerHTML =`<div class="row ">
+        div.innerHTML = `<div class="row ">
                         <div class="col-12 d-flex justify-content-center align-items-center">
                             <img class=" mx-auto" src="${phone.image}" class="card-img-top" alt="...">
                             <div class="card-body ">
@@ -30,41 +27,68 @@ const displayData = phones =>{
 
         phoneCard.appendChild(div);
     });
-
-
 }
 
 // calling API with arrow function to display details of phone
 const loadDetails = details => {
     const url = `https://openapi.programming-hero.com/api/phone/${details}`
-   
+
     fetch(url)
-    .then (res => res.json())
-    .then (data => displayDetails(data.data));
+        .then(res => res.json())
+        .then(data => displayDetails(data.data));
 }
 
 // show phone details sector using arrow function & object
-const displayDetails = info =>{
-    console.log(info)
+const displayDetails = info => {
+    // console.log(info)
     const moreDetails = document.getElementById('more-details')
-    moreDetails.innerHTML = `<div class="col-md-6 ">
-                            <div class=" card rounded-3  mb-3 p-4 w-50 mx-auto border-0 " style ="background :rgb(247, 241, 231)" >
+    moreDetails.innerHTML = `<div class="col-md-12">
+                            <div class=" card rounded-3  mb-3 p-4 w-50 mx-auto border-0 " style ="background :rgb(245, 240, 231)" >
                                 <img class="w-25  mx-auto" src="${info.image}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                 <h4 class="card-title ">${info.name}</h4>
-                                <h5 class="card-title">Release Date : ${info.releaseDate}</h5>
-                                
-                                <h5 class="card-title "> Main Features :- </h5> 
-                                    <ul>
+                                <h5 class="card-title">Release Date : ${info.releaseDate}</h5>                      
+                                <h5 class="card-title "> Main Features :-</h5> 
+                                <ul>
                                     <li> ${info.mainFeatures.chipSet} </li>
                                     <li>${info.mainFeatures.displaySize}</li>
                                     <li>${info.mainFeatures.memory}</li>
+                                    <li>${info.mainFeatures.storage}</li>
                                 </ul>
-                                <button onclick="" class="btn btn-outline-secondary " type="button"> Sensor info </button>
-                                <button onclick="" class="btn btn-outline-secondary" type="button"> Others info</button>
-                               
+                                <button onclick="sensor('${info.mainFeatures.sensors}')" class="btn btn-outline-secondary " type="button"> Sensor info </button>
+                                <button onclick="others('${info.others}')" class="btn btn-outline-secondary" type="button"> Others info</button>        
                                 </div>
                                 </div>
-                                </div>`; 
+                                </div>`;
 
 }
+
+const sensor = sensorData => {
+    const sensorInfo = document.getElementById('sensor-info');
+    sensorInfo.innerHTML = `<div class="col-md-12">
+                                <div class=" card rounded-3  mb-3 p-4 w-50 mx-auto border-0 " style ="background :rgb(234, 255, 255)" >
+                                    <div class="card-body">                  
+                                       <h5 class="card-title "> sensor information :-</h5> 
+                                    <ul>
+                                        <li>${sensorData}</li>                         
+                                    </ul>
+                                    </div>
+                                </div>
+                                </div>`;
+}
+
+const others = othersData => {
+    const othersInfo = document.getElementById('others-info');
+    othersInfo.innerHTML = `<div class="col-md-12">
+                                <div class=" card rounded-3  mb-3 p-4 w-50 mx-auto border-0 " style ="background :rgb(234, 255, 255)" >
+                                    <div class="card-body">                  
+                                       <h5 class="card-title "> Others information :-</h5> 
+                                    <ul>
+                                        <li>${othersData}</li>                                                                                
+                                    </ul>
+                                    </div>
+                                </div>
+                                </div>`;
+}
+
+
